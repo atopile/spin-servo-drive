e6f8112a5d9a1fa8d99522fdfb1e1199a848de2a
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-07-07 23:18:25 -0700

    Motor's spinning baby!

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,14 +1,53 @@
 #include <Arduino.h>
+#include <SimpleFOC.h>
 
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
+// BLDC motor & driver instance
+// BLDCMotor motor = BLDCMotor(pole pair number, phase resistance (optional) );
+BLDCMotor motor = BLDCMotor(4);
+
+// BLDCDriver3PWM driver = BLDCDriver3PWM(pwmA, pwmB, pwmC, Enable(optional));
+BLDCDriver6PWM driver = BLDCDriver6PWM(5, 6, 9, 10, 11, 12);
+
+// instantiate the commander
+Commander command = Commander(Serial);
+void doTarget(char* cmd) { command.scalar(&motor.target, cmd); }
+
+void setup() {
+
+  // driver config
+  // power supply voltage [V]
+  driver.voltage_power_supply = 10;
+  driver.init();
+  // link the motor and the driver
+  motor.linkDriver(&driver);
+
+  // limiting motor movements
+  // motor.phase_resistance = 3.52 // [Ohm]
+  // motor.current_limit = 2;   // [Amps] - if phase resistance defined
+  motor.voltage_limit = 2;   // [V] - if phase resistance not defined
+  motor.velocity_limit = 5; // [rad/s] cca 50rpm
+
+  // open loop control config
+  motor.controller = MotionControlType::velocity_openloop;
+
+  // init motor hardware
+  motor.init();
+
+  // add target command T
+  command.add('T', doTarget, "target velocity");
+
+  Serial.begin(115200);
+  Serial.println("Motor ready!");
+  Serial.println("Set target velocity [rad/s]");
+  _delay(1000);
 }
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+void loop() {
+
+  // open loop velocity movement
+  // using motor.voltage_limit and motor.velocity_limit
+  motor.move();
+
+  // user communication
+  command.run();
 }
